175f30320650399361f465c185b3ac60ff8113c4
heinhtet <heinhtetstarfall@gmail.com>
2017-03-02 00:29:02 +0630

    fixed migration generators

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -14,13 +14,21 @@ class AspectSetupTables extends Migration
     public function up()
     {
     	 
-        Schema::create({{ $roles }}, function (Blueprint $table) {
+       
+    	Schema::create({{ $roles }}, function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
         });
-    	
-    	
+
+        Schema::create({{ $roles_permission }}, function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id');
+            $table->string('controller');
+            $table->string('method');
+            $table->boolean('status');
+            $table->timestamps();
+        });
 
     }
 
